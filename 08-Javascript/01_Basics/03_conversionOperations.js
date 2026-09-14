b8a0aa3ab3cd44a6d1824da54d76451ef9597fb8
Yusuf.M <ymotiwala19@gmail.com>
2026-09-14 13:50:08 +0000

    Studied conversions and operations

---
+++ 08-Javascript/01_Basics/03_conversionOperations.js
@@ -0,0 +1,56 @@
+/* let score = "abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true/false => 1/0
+
+let isLoggenIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggenIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+*/
+// ******** Operations *******
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let st1 = "Hello"
+let st2 = " Yusuf"
+
+console.log(st1 + st2);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2") // 32
+// Which conversion is first is applied to all 
+
+console.log(+true); // 1
+// console.log(true+); Error
+
+
+
+
+
+
+
+
+
+
