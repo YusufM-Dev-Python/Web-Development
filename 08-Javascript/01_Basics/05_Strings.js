40a77df86fead007035408ccc26875741c61d3e7
Yusuf.M <ymotiwala19@gmail.com>
2026-09-15 10:56:45 +0000

    Strings, Math and nums studied

---
+++ 08-Javascript/01_Basics/05_Strings.js
@@ -0,0 +1,8 @@
+const name = "Yusuf"
+const repoCount = 6
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Yusuf")
---
+++ 08-Javascript/01_Basics/06_nums_maths.js
@@ -0,0 +1,47 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); // 2 digits after decimal 
+
+const otherNum = 123.8966
+console.log(otherNum.toPrecision(3)); // 3 digits precision and returns a string 
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')); // acc to indian standards, if parameter empty then upto US standard
+
+// ++++++ MATHS ++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(3,4,5,6));
+console.log(Math.max(3,4,5,6));
+
+console.log(Math.random()); // only from 0-1
+console.log(Math.floor(Math.random()*10) + 1); // 1-10
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min); // to prevent overflow
+
+
+
+
+
+
+
+
+
+
+
+
+
+
